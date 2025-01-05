9e6a4c74048eaa7ae80181acbb6a99e2b95fc91f
anser624 <am24626526jan@gmail.com>
2025-01-05 14:37:20 +0500

    files added

---
+++ app.js
@@ -0,0 +1,11 @@
+let box = document.querySelector('#box');
+let input = document.querySelector('#input');
+let inputbrdr = document.querySelector('#input-brdr');
+
+input.addEventListener('input', function() {
+    // box.style.borderRadius = input.value + 'px';
+    box.style.background = input.value;
+});
+inputbrdr.addEventListener('input', function() {
+    box.style.borderRadius = inputbrdr.value + 'px';
+});
